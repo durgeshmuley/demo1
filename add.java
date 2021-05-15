7dd6d2c18ab579dcc5b6bfb8727afe9e6df7d362
durgesh muley <84194088+durgeshmuley@users.noreply.github.com>
2021-05-15 15:21:08 +0530

    Add files via upload

---
+++ add.java
@@ -0,0 +1,12 @@
+class math
+{
+public static void main(String args[]){
+
+System.out.println("sum of these no is : ");
+int num1=10;
+int num2=10;
+int num3=10;
+int sum=num1+num2+num3;
+System.out.print(sum);
+}
+}  
